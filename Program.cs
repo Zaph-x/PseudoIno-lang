017ab11ebd2c2edc0f8d19be2297016488fa9666
Zaph-x <mikkel@luceartfoto.dk>
2020-02-25 14:19:59 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Compiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
