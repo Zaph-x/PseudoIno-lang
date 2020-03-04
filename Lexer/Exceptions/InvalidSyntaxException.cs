d10d0412e7ee2932d59800e7060c7fd035be0504
Zaph-x <mikkel@luceartfoto.dk>
2020-03-04 20:24:07 +0100

    Made exception public

--- Lexer/Exceptions/InvalidSyntaxException.cs
+++ Lexer/Exceptions/InvalidSyntaxException.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Lexer.Exceptions
 {
-    class InvalidSyntaxException : Exception
+    public class InvalidSyntaxException : Exception
     {
         public InvalidSyntaxException(string message) : base(message)
         {
