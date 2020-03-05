aca96e6760af87a7ea3a0e606663bc9c67b5832a
Zaph-x <mikkel@luceartfoto.dk>
2020-03-05 11:50:58 +0100

    Updated recogniser to not generate anything

--- Lexer/Tokenizer.cs
+++ Lexer/Tokenizer.cs
@@ -14,43 +14,43 @@ namespace Lexer
         
         public Tokenizer(string inputFile)
         {
-            Recogniser recogniser = new Recogniser();
-            if (File.Exists(inputFile))
-            {
-                Lines = recogniser.ReadFile(inputFile);
-                foreach (var line in Lines)
-                {
-                    List <string> bits = recogniser.SplitString(line);
-                    foreach (var element in bits)
-                    {
-                        Elements.Add(element);
-                    }
-                }
-            }
-            else
-            {
-                throw new FileNotFoundException();
-            }
-
-            foreach (var element in Elements)
-            {
-                if (recogniser.IsKeyword(element))
-                {
-                    Tokens.Add(new Token(Keywords.Keys[element],element,0,0));
-                }
-                else if (recogniser.IsDigit(element) && element != "a")
-                {
-                    //Dummy token hardcoded
-                    Tokens.Add(new Token(TokenType.NUMERIC,"5",0,0));
-                }
-                else
-                {
-                    Tokens.Add(new Token(TokenType.VAR,"a",0,0));
-                }
-
-                //check digit
-                //check id
-            }
+            // Recogniser recogniser = new Recogniser();
+            // if (File.Exists(inputFile))
+            // {
+                // Lines = recogniser.ReadFile(inputFile);
+                // foreach (var line in Lines)
+                // {
+                    // List <string> bits = recogniser.SplitString(line);
+                    // foreach (var element in bits)
+                    // {
+                        // Elements.Add(element);
+                    // }
+                // }
+            // }
+            // else
+            // {
+                // throw new FileNotFoundException();
+            // }
+//
+            // foreach (var element in Elements)
+            // {
+                // if (recogniser.IsKeyword(element))
+                // {
+                    // Tokens.Add(new Token(Keywords.Keys[element],element,0,0));
+                // }
+                // else if (recogniser.IsDigit(element) && element != "a")
+                // {
+                    // Dummy token hardcoded
+                    // Tokens.Add(new Token(TokenType.NUMERIC,"5",0,0));
+                // }
+                // else
+                // {
+                    // Tokens.Add(new Token(TokenType.VAR,"a",0,0));
+                // }
+//
+                // check digit
+                // check id
+            // }
         }
     }
 }
--- Tests/Lexer.Tests/RecogniserTest.cs
+++ Tests/Lexer.Tests/RecogniserTest.cs
@@ -10,32 +10,17 @@ namespace Lexer.Tests
 {
     public class RecogniserTest
     {
+        private string FakeContent = "Hello, World!";
+        private byte[] FakeUTF8Bytes;
         Recogniser recogniser = new Recogniser();
-
+        private MemoryStream FakeUTF8Stream = new MemoryStream(FakeUTF8Bytes);
+        private StreamReader FakeReader;
         [OneTimeSetUp]
         public void OneTimeSetup()
         {
+            FakeUTF8Bytes = Encoding.UTF8.GetBytes(FakeContent);
+            FakeReader = new StreamReader(FakeUTF8Stream, Encoding.UTF8, false);
             onetime();
-        }
-
-        [Test]
-        public void Test_InputString_RecogniseStringInLanguage()
-        {
-            Assert.AreEqual(0, recogniser.InputString("a is 4"), "Recogniser did not recognise the string");
-        }
-
-        [Test]
-        public void Test_InputString_DoNotRecogniseMalformedString()
-        {
-            Assert.AreNotEqual(0, recogniser.InputString("a 2 is"), "Recogniser recognised the string when it should not");
-
-        }
-
-        [Test]
-        public void Test_InputString_DoNotRecogniseStringNotInLanguage()
-        {
-            Assert.AreNotEqual(0, recogniser.InputString("neiojsfj3324"), "Recogniser recognised the string when it should not");
-
         }
         
         [Test]
@@ -60,120 +45,6 @@ namespace Lexer.Tests
         public void Test_IsAcceptedCharacter_RejectDigits()
         {
             Assert.IsFalse(recogniser.IsAcceptedCharacter('4'), "The character '4' was not rejected when it should be");
-        }
-
-        [Test]
-        public void Test_ScanDigit_CanScanIntegers()
-        {
-            Assert.AreEqual(2222, recogniser.ScanDigit("2222"), "Recogniser did not recognise the digit (Int)");
-        }
-
-        [Test]
-        public void Test_WrongInputShouldThrowException()
-        {
-            Assert.Throws<InvalidSyntaxException>(() =>
-            {
-                recogniser.ScanDigit("NotAValidDigit");
-            }, "Recogniser failed to catch invalid digit");
-
-        }
-
-        [Test]
-        public void Test_ScanDigit_CanScanFloats()
-        {
-            Assert.AreEqual(1.1f, recogniser.ScanDigit("1.1"), "Recogniser did not recognise the digit (Float)");
-        }
-
-        [Test]
-        public void Test_ScanDigit_CanRemoveWhitespaces()
-        {
-            Assert.AreEqual(22, recogniser.ScanDigit("                22"), "Recogniser did not recognise the digit (Int)");
-        }
-
-        [Test]
-        public void Test_ScanDigit_CanRemoveWhitespacesForFloats()
-        {
-            Assert.AreEqual(111.1011f, recogniser.ScanDigit("      111.1011"), "Recogniser did not recognise the digit (Float)");
-        }
-        [Test]
-        public void Test_ScanDigit_CanScanNegativeDigits()
-        {
-            Assert.AreEqual(-2222, recogniser.ScanDigit("-2222"), "Recogniser did not recognise the digit (Int)");
-        }
-
-        [Test]
-        public void Test_ScanDigit_CanScanNegativeFloats()
-        {
-            Assert.AreEqual(-1.1f, recogniser.ScanDigit("-1.1"), "Recogniser did not recognise the digit (Float)");
-        }
-
-        [Test]
-        public void Test_SplitCountString_ReturnsCorrectAmountOfStrings()
-        {
-            string inputString = "a is 1";
-            List<string> substrings = recogniser.SplitString(inputString);
-            Assert.AreEqual(3, substrings.Count, "The recogniser did not find the correct amount of substrings");
-        }
-
-
-        [Test]
-        public void Test_SplitCountString_ReturnsCorrectWithExtraWhiteSpace()
-        {
-            string inputString = "a           is  1";
-            List<string> substrings = recogniser.SplitString(inputString);
-            Assert.AreEqual(3, substrings.Count, "Whitespaces were not ignored.");
-        }
-
-        [Test]
-        public void Test_SplitCountString_ReturnsZeroOnEmptyString()
-        {
-            string inputString = "";
-            List<string> substrings = recogniser.SplitString(inputString);
-            Assert.AreEqual(0, substrings.Count, "Emptystring was recognised as a string");
-        }
-
-        [Test]
-        public void Test_SplitCountString_DoNotRecogniseNewLine()
-        {
-            string inputString = "test \n test";
-            List<string> substrings = recogniser.SplitString(inputString);
-            Assert.AreNotEqual(2, substrings.Count, "Newline was recognised as part of the string");
-        }
-
-        [Test]
-        public void Test_ReadFile_ReturnsCorrectAmountOfLines()
-        {
-
-            List<string> lines = recogniser.ReadFile("fileWith10Lines");
-            Assert.AreEqual(10, lines.Count, "File did not contain 10 lines when it should.");
-        }
-
-        [Test]
-        public void Test_FileLineCount_ReturnsNonZeroAmountOfLines()
-        {
-            List<string> lines = recogniser.ReadFile("fileWith10Lines");
-            Assert.AreNotEqual(0, lines.Count, "File contained 0 lines when it should contain more.");
-        }
-
-        // Helper functions
-
-        public void onetime()
-        {
-            using (FileStream fs = File.Create("fileExist")) { }
-
-            List<string> lines = new List<string>();
-            for (int i = 0; i < 10; i++)
-            {
-                lines.Add(i.ToString());
-            }
-
-            using (StreamWriter file = new StreamWriter("fileWith10Lines"))
-            {
-                foreach (string line in lines)
-                {
-                    file.WriteLine(line);
-                }
-            }
         }
     }
 }
