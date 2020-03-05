237591ea1b6b8e7f96c88b77591065fb7c9900e4
sejbas <markamb2497@gmail.com>
2020-03-05 11:11:17 +0100

    Added constructor for string list

--- Lexer/Tokenizer.cs
+++ Lexer/Tokenizer.cs
@@ -12,25 +12,13 @@ namespace Lexer
         
         public List<Token> Tokens = new List<Token>();
         
+        Recogniser recogniser = new Recogniser();
+        
         public Tokenizer(string inputFile)
         {
-            Recogniser recogniser = new Recogniser();
-            if (File.Exists(inputFile))
-            {
-                Lines = recogniser.ReadFile(inputFile);
-                foreach (var line in Lines)
-                {
-                    List <string> bits = recogniser.SplitString(line);
-                    foreach (var element in bits)
-                    {
-                        Elements.Add(element);
-                    }
-                }
-            }
-            else
-            {
-                throw new FileNotFoundException();
-            }
+            recogniser = new Recogniser();
+            
+            FileToElements(inputFile);
 
             foreach (var element in Elements)
             {
@@ -50,6 +38,59 @@ namespace Lexer
 
                 //check digit
                 //check id
+            }
+        }
+        
+        public Tokenizer(List<string> inputFile)
+        {
+            recogniser = new Recogniser();
+
+            Lines = inputFile;
+            LinesToElements();
+
+            foreach (var element in Elements)
+            {
+                if (recogniser.IsKeyword(element))
+                {
+                    Tokens.Add(new Token(Keywords.Keys[element],element,0,0));
+                }
+                else if (recogniser.IsDigit(element) && element != "a")
+                {
+                    //Dummy token hardcoded
+                    Tokens.Add(new Token(TokenType.NUMERIC,"5",0,0));
+                }
+                else
+                {
+                    Tokens.Add(new Token(TokenType.VAR,"a",0,0));
+                }
+
+                //check digit
+                //check id
+            }
+        }
+
+        private void FileToElements(string inputFile)
+        {
+            recogniser = new Recogniser();
+            if (File.Exists(inputFile))
+            {
+                Lines = recogniser.ReadFile(inputFile);
+            }
+            else
+            {
+                throw new FileNotFoundException();
+            }
+        }
+
+        private void LinesToElements()
+        {
+            foreach (var line in Lines)
+            {
+                List <string> bits = recogniser.SplitString(line);
+                foreach (var element in bits)
+                {
+                    Elements.Add(element);
+                }
             }
         }
     }
