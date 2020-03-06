76654840b445c3d21119649e2f3f04fc7dc6b885
Zaph-x <mikkel@luceartfoto.dk>
2020-03-06 08:36:26 +0100

    [skip travis] Wrote documentation

--- Lexer/Tokenizer.cs
+++ Lexer/Tokenizer.cs
@@ -8,8 +8,6 @@ namespace Lexer
 {
     public class Tokenizer
     {
-       /*private List<string> Lines;
-         private List<string> Elements = new List<string>();*/
         public List<Token> Tokens = new List<Token>();
         
         Recogniser recogniser = new Recogniser();
@@ -20,18 +18,34 @@ namespace Lexer
         private int Offset {get;set;}
         private StreamReader reader;
 
+        /// <summary>
+        /// The constructor for the Tokenizer class. This will set the iniitiate a reader and a recogniser.
+        /// </summary>
+        /// <param name="stream">The StreamReader responsible for reading a class</param>
         public Tokenizer(StreamReader stream)
         {
             recogniser = new Recogniser();
             reader = stream;
         }
         
+        /// <summary>
+        /// A function to continously return the current character.
+        /// </summary>
+        /// <returns>
+        /// The current character
+        /// </returns>
         public char Current()
         {
             return CurrentChar;
         }
 
-        public Lexer.Objects.Token Token(TokenType type, string val)
+        /// <summary>
+        /// A function to generate a token.
+        /// </summary>
+        /// <returns>
+        /// A token from the value.
+        /// </returns>
+        public Token Token(TokenType type, string val)
         {
             return new Token(type, val, Line, Offset);
         }
@@ -39,10 +53,13 @@ namespace Lexer
         /// <summary>
         /// Sets current character as the next character in the stream and advances in the stream.
         /// </summary>
+        /// <returns>
+        /// The next character in the stream.
+        /// </returns>
         public char Pop()
         {
             CurrentChar = (char)reader.Read();
-            if (CurrentChar == '\n')
+            if (IsEOL())
                 Line++;
             return CurrentChar;
         }
@@ -50,17 +67,29 @@ namespace Lexer
         /// <summary>
         /// Peeks the next character in the stream and sets NextChar to the value
         /// </summary>
+        /// <returns>
+        /// The next character.
+        /// </returns>
         public char Peek()
         {
             NextChar = (char)reader.Peek();
             return NextChar;
         }
 
+        /// <summary>
+        /// Checks if the current character is end of line.
+        /// <summary>
+        /// <returns>
+        /// True if the current character is end of line, otherwise false.
+        /// </returns>
         private bool IsEOL()
         {
             return CurrentChar == '\n';
         }
 
+        /// <summary>
+        /// A function to to generate tokens. This is done by reading from the stream.
+        /// </summary>
         public void GenerateTokens()
         {
             string subString = "";
