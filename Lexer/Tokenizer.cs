a976890abe1c33f39627c33ff207d8cb1e952dd9
Zaph-x <mikkel@luceartfoto.dk>
2020-03-07 10:44:52 +0100

    Documented every function within the tokenizer

--- Lexer/Tokenizer.cs
+++ Lexer/Tokenizer.cs
@@ -46,14 +46,30 @@ namespace Lexer
         }
 
         /// <summary>
-        /// A function to generate a token.
+        /// A function to generate a token with a value. 
+        /// This will be used for tokens where it is imperative that the value is carried over to the target language.
         /// </summary>
+        /// <param name="type">The tokentype to return</param>
+        /// <param name="val">The value of the token</param>
         /// <returns>
         /// A token from the value.
         /// </returns>
         public Token Token(TokenType type, string val)
         {
             return new Token(type, val, Line, Offset);
+        }
+
+        /// <summary>
+        /// A function to generate a token without a value.
+        /// This will be used to generate tokens where the value does not have to be carried over to the target language
+        /// </summary>
+        /// <param name="type">The token type</param>
+        /// <returns>
+        /// A token without a value.
+        /// </returns>
+        public Token Token(TokenType type)
+        {
+            return new Token(type, "", Line, Offset);
         }
 
         /// <summary>
@@ -115,32 +131,79 @@ namespace Lexer
         {
             return CurrentChar == '\n';
         }
-
+        /// <summary>
+        /// Checks if a character is the end of line.
+        /// </summary>
+        /// <param name="character">The character to check</param>
+        /// <returns>
+        /// True if the current character is end of line, otherwise false.
+        /// </returns>
         private bool IsEOL(char character)
         {
             return character == '\n';
         }
 
+        /// <summary>
+        /// Checks if the current character is the end of file character
+        /// </summary>
+        /// <returns>
+        /// True if the current character is end of file, otherwise false.
+        /// </returns>
         private bool IsEOF()
         {
             return (int)CurrentChar == '\uffff';
         }
 
+        /// <summary>
+        /// Checks if a given character is the end of file character
+        /// </summary>
+        /// <param name="character">The character to check</param>
+        /// <returns>
+        /// True if the character is the end of file character, otherwise false.
+        /// </returns>
         private bool IsEOF(char character)
         {
             return (int)character == '\uffff';
         }
 
+        /// <summary>
+        /// Checks if the current character is a whitespace
+        /// </summary>
+        /// <returns>
+        /// true if the current character is a whitespace, otherwise false
+        /// </returns>
         private bool IsSpace()
         {
             return CurrentChar == ' ';
         }
+
+        /// <summary>
+        /// Checks if a given character is a whitespace
+        /// </summary>
+        /// <param name="character">The character to check</param>
+        /// <returns>
+        /// True if the character is a whitespace, otherwise false.
+        /// </returns>
         private bool IsSpace(char character)
         {
             return character == ' ';
         }
 
-        private void ScanNumeric()
+        /// <summary>
+        /// This function will scan a secquence of characters beginning with a digit.
+        /// The function will traverse the stream until it sees a character that is no longer recognized as a digit.
+        /// If the non-digit character is a period (.) and it is the first period in the sequence, the function will treat the following digits as floating points.
+        /// If there are no digits after the period character, the scanner will append a 0 to the value, to ensure the value is a float in the target language.
+        /// <example>The following sequences are recognized as numeric:
+        /// <code>
+        /// 2
+        /// 42
+        /// 42.1
+        /// 54. # Will become 54.0
+        /// </code>
+        /// </example>
+        /// </summary>
+        private void ScanNumeric() 
         {
             string subString = CurrentChar.ToString();
             while (recogniser.IsDigit(Peek()))
@@ -163,14 +226,15 @@ namespace Lexer
                 {
                     subString += "0";
                 }
-                if (!IsEOL(NextChar) && !IsEOF(NextChar) && !IsSpace(NextChar))
-                {
-                    throw new InvalidSyntaxException($"Numeric literal can only contain numbers. Found '{NextChar}({(int)NextChar})'. Error at line {Line}:{Offset}.");
-                }
             }
             Tokens.Add(Token(TokenType.NUMERIC, subString));
         }
 
+        /// <summary>
+        /// This function will scan a sequence of characters providing a range token to the token list.
+        /// The function will traverse while the next character is a period (.) symbol.
+        /// <exception cref="InvalidSyntaxException">When the traversed sequence does not produce a token with the value of exavtly '..'</exception>
+        /// </summary>
         private void ScanRange()
         {
             string subString = CurrentChar.ToString();
@@ -184,14 +248,25 @@ namespace Lexer
             {
                 throw new InvalidSyntaxException($"Invalid range symbol. Range symbol must be '..' but was '{subString}'. Error at line {Line}:{Offset}.");
             }
-            Tokens.Add(Token(TokenType.RANGE, subString));
+            Tokens.Add(Token(TokenType.RANGE));
         }
 
+        /// <summary>
+        /// This function will scan a sequence of characters, providing a string token.
+        /// The function will traverse the sequence, until it meets a quotation (") symbol.
+        /// <example>An example of a valid string:
+        /// <c>
+        /// "Hello, World!"
+        /// "I am a valid string,.!-/()*?#$"
+        /// </c>
+        /// </example>
+        /// <exception cref="InvalidSyntaxExecption">When the traversed sequence is not closed by a quotation symbol</exception>
+        /// </summary>
         private void ScanString()
         {
             string subString = CurrentChar.ToString();
 
-            while (Peek() != '"')
+            while (Peek() != '"' && !IsEOF(Peek()))
             {
                 Pop();
                 subString += CurrentChar;
@@ -203,6 +278,10 @@ namespace Lexer
             Tokens.Add(Token(TokenType.STRING, subString));
         }
 
+        /// <summary>
+        /// This function will scan a sequence of characters, providing a comment token.
+        /// The function will traverse the sequence, until it reaches the end of line or end of file character.
+        /// </summary>
         private void ScanComment()
         {
             string subString = CurrentChar.ToString();
@@ -214,6 +293,18 @@ namespace Lexer
             Tokens.Add(Token(TokenType.COMMENT, subString));
         }
 
+        /// <summary>
+        /// This function will scan a sequence of characters, providing a multiline comment token.
+        /// The function will traverse the sequence, until it meets a sequence representing the end of a multiline comment.
+        /// <example>An example of a valid multiline comment:
+        /// <c>
+        /// <# This is a valid multiline comment #>
+        /// <# This is also
+        /// a valid multiline comment#>
+        /// </c>
+        /// </example>
+        /// <exception cref="InvalidSyntaxExecption">When the traversed sequence is not closed by a quotation symbol</exception>
+        /// </summary>
         private void ScanMultiLineComment()
         {
             string subString = CurrentChar.ToString();
@@ -229,7 +320,22 @@ namespace Lexer
             Tokens.Add(Token(TokenType.MULT_COMNT, subString));
         }
 
-        private void ScanCharacter()
+        /// <summary>
+        /// This function will scan a sequence of characters providing either a VAR token, a KEYWORD token, an INDEX token, a QUESTIONMARK token, or any ARRAY token.
+        /// This is done by traversing the sequence of characters until it meets either a newline or any of the symbols representing any of the rest of the tokens.
+        /// The time to look up a keyword is O(1) as this is done by using a hashed value in a dictionary.
+        /// This function can produce multiple tokens within itself.
+        /// <example> An example of possible accepted tokens:
+        /// <c>
+        /// a # Variable name
+        /// is # Keyword
+        /// for # Keyword
+        /// a@2 # While the 2 is not recognised in this function this will result in tokens "VAR ARRAYINDEX NUMERIC"
+        /// a[4] # This will result in the tokens "VAR ARRAYLEFT NUMERIC ARRAYRIGHT"
+        /// </c>
+        /// </example>
+        /// </summary>
+        private void ScanWord()
         {
             string subString = CurrentChar.ToString();
             while (recogniser.IsAcceptedCharacter(Peek()))
@@ -272,14 +378,42 @@ namespace Lexer
                 Pop();
                 Tokens.Add(Token(TokenType.ARRAYRIGHT, "]"));
             }
-
-            /*if (Peek() != ' ' || Peek() != '\n')
-            {
-                throw new InvalidSyntaxException($"Illegal symbol. Error at line {Line}:{Offset}");
-            }*/
         }
+
         /// <summary>
-        /// A function to to generate tokens. This is done by reading from the stream.
+        /// This function will add an operator token to the token list.
+        /// If the current character is a plus, a plus operator token will be added
+        /// If the current character is a minus, a minus operator will be added
+        /// If the current character is an asterisk, a times operator will be added
+        /// If the current character is a forward slash, a divide operator will be added
+        /// If the current character is a percentage symbol, a modulo operator will be added
+        /// </summary>
+        private void ScanOperators()
+        {
+            switch (CurrentChar)
+            {
+                case '+':
+                    Tokens.Add(Token(TokenType.OP_PLUS));
+                    break;
+                case '-':
+                    Tokens.Add(Token(TokenType.OP_MINUS));
+                    break;
+                case '*':
+                    Tokens.Add(Token(TokenType.OP_TIMES));
+                    break;
+                case '/':
+                    Tokens.Add(Token(TokenType.OP_DIVIDE));
+                    break;
+                case '%':
+                    Tokens.Add(Token(TokenType.OP_MODULO));
+                    break;
+                default:
+                    throw new InvalidSyntaxException($"'{CurrentChar}' was not recognised as a valid operator. Error at line {Line}:{Offset}.")
+            }
+        }
+
+        /// <summary>
+        /// A function to to generate tokens. This is done by reading from the stream and using any of the scan functions.
         /// </summary>
         public void GenerateTokens()
         {
@@ -289,11 +423,12 @@ namespace Lexer
                 Pop();
                 if (IsSpace()) { continue; }
                 else if (recogniser.IsDigit(CurrentChar)) { ScanNumeric(); }
-                else if (recogniser.IsAcceptedCharacter(CurrentChar)) { ScanCharacter(); }
+                else if (recogniser.IsAcceptedCharacter(CurrentChar)) { ScanWord(); }
                 else if (CurrentChar == '.') { ScanRange(); }
-                else if (CurrentChar == '_' && (recogniser.IsAcceptedCharacter(Peek()) || recogniser.IsDigit(Peek()))) { ScanCharacter(); }
+                else if (CurrentChar == '_' && (recogniser.IsAcceptedCharacter(Peek()) || recogniser.IsDigit(Peek()))) { ScanWord(); }
                 else if (CurrentChar == '#') { ScanComment(); }
                 else if (CurrentChar == '<' && Peek() == '#') { ScanMultiLineComment(); }
+                else if ("+-*/%".Contains(CurrentChar)) { ScanOperators(); }
 
             }
 
