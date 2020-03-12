4c92f4982277302e165eb0d7e66d2736748f59bc
sejbas <markamb2497@gmail.com>
2020-03-12 09:03:54 +0100

    Started on creating empty methods for Parsenizer class

---
+++ Parser/Parsenizer.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using Lexer.Objects;
+
+namespace Parser
+{
+    public class Parsenizer
+    {
+        // public AST Ast = new AST();
+        
+        public Parsenizer()
+        {
+            // Empty
+        }
+
+        public void CreateAndFillAST(List<Token> tokens)
+        {
+            // Create AST and fill with tokens
+        }
+
+        public void TypeCheckAST()
+        {
+            // Do type checking
+        }
+
+        public void CreateAndFillSymbolTable()
+        {
+            // Create symbol table
+        }
+    }
+}
