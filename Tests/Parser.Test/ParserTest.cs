db2641d5311c62f07f78dc6d69fe7ebef02d521a
sejbas <markamb2497@gmail.com>
2020-03-12 16:54:16 +0100

    Created file for tests

---
+++ Tests/Parser.Test/ParserTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Parser.Test
+{
+    public class ParserTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test_ParseTable_Apply()
+        {
+            Assert.Pass();
+        }
+    }
+}
