c94f9dc6dace95f1192df129b652700ca1b015f4
Zaph-x <mikkel@luceartfoto.dk>
2020-03-18 15:12:48 +0100

    Changed to correct namespace

--- Core/Objects/CommandLineOptions.cs
+++ Core/Objects/CommandLineOptions.cs
@@ -1,4 +1,4 @@
-namespace Core
+namespace Objects.Core
 {
     public class CommandLineOptions
     {
--- Core/Objects/FileChecker.cs
+++ Core/Objects/FileChecker.cs
@@ -2,7 +2,7 @@ using System.Text;
 using System.IO;
 using Core.Exceptions;
 
-namespace Core
+namespace Objects.Core
 {
     public class FileChecker
     {
