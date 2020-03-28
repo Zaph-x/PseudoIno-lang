8352afdb272515bd7429b8fdd4cb0b68beec6b8f
smh87 <spm2014@hotmail.com>
2020-03-28 21:18:46 +0100

    Lavet liste om til tuple vi dropper classen med symboltable. mangler unit test

--- Parser/Objects/SymbolTableBuilder.cs
+++ Parser/Objects/SymbolTableBuilder.cs
@@ -8,18 +8,27 @@ namespace Parser.Objects
 {
     class SymbolTableBuilder
     {
-        int Current;
-        //list for lookup of scope and name to see if there are duplicates
-        List<SymbolTable> ScopeTracker = new List<SymbolTable>();
+        //current level scope
+        int currenLevel=0;
+        public int CurrentLevelProp
+        {
+            get { return currenLevel; }
+            set { currenLevel = value; }
+        }
+
+
+        //Dictionary for lookup of scope and name to see if there are duplicates. The dictionary also have open and close params.
+        Dictionary<string,Tuple<int,bool,bool>> ScopeTracker = new Dictionary<string, Tuple<int,bool, bool>>();
         //List for symboltable content name and type
         List<Dictionary<string, TokenType>> Symboltable = new List<Dictionary<string, TokenType>>(); 
         void OpenScope()
         {
          
         }
-        void CloseScope()
+        void CloseScope(string name)
         {
-         
+            ScopeTracker[name] = Tuple.Create(ScopeTracker[name].Item1, ScopeTracker[name].Item2, true);
+            CurrentLevelProp++;
         }
         public TokenType RetrieveSymbol(string name)
         {
@@ -36,9 +45,15 @@ namespace Parser.Objects
         /// <param name="type"></param>
         public void AddSym(string name, TokenType type)
         {
-           if(LookUp(name,Current)==true)
+           
+           if(LookUp(name,CurrentLevel)==true)
             {
-                Symboltable.Add(new Dictionary<string, TokenType> ;
+                //add new symboltable
+                Symboltable.Add(new Dictionary<string, TokenType>());
+                Symboltable[Symboltable.Count - 1].Add(name, type);
+                //Add name, level, open, close param
+                ScopeTracker.Add(name, Tuple.Create(CurrentLevel,true, false));
+            
             }
             //if (DicSymbolTable.TryAdd(name, type))
             //{
@@ -48,10 +63,10 @@ namespace Parser.Objects
         }
         public bool LookUp(string Name, int Level)
         {
-      
-            if (ScopeTracker.Contains(new SymbolTable(Level, Name)))
+
+            if (ScopeTracker.ContainsKey(Tuple.Create(Name, Level)))
             {
-                throw new InvalidSyntaxException($"Symbol { Name } was not added in the symbol table, since it exist allready there at scope {Level}");
+                throw new InvalidSyntaxException($"Symbol { Name } was not added in the symbol table, since it exist already at scope {Level}");
             }
             return true;
         }
