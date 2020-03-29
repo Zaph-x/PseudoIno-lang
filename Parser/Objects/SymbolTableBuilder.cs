6bcef879dab8f00f45d94b5966055cf843900ec1
smh87 <spm2014@hotmail.com>
2020-03-29 10:38:44 +0200

    rettelser addsymboltalble

--- Parser/Objects/SymbolTableBuilder.cs
+++ Parser/Objects/SymbolTableBuilder.cs
@@ -3,6 +3,7 @@ using Lexer.Exceptions;
 using Lexer.Objects;
 using System.Collections;
 using System;
+using System.Linq;
 
 namespace Parser.Objects
 {
@@ -28,7 +29,7 @@ namespace Parser.Objects
         void CloseScope(string name)
         {
             ScopeTracker[name] = Tuple.Create(ScopeTracker[name].Item1, ScopeTracker[name].Item2, true);
-            CurrentLevelProp++;
+            
         }
         public TokenType RetrieveSymbol(string name)
         {
@@ -45,30 +46,32 @@ namespace Parser.Objects
         /// <param name="type"></param>
         public void AddSym(string name, TokenType type)
         {
-           
-           if(LookUp(name,CurrentLevel)==true)
-            {
+
+            if (LookUp(name, CurrentLevelProp) == false && Symboltable.Any(x=>x.ContainsKey(name))==false)
+                {
                 //add new symboltable
                 Symboltable.Add(new Dictionary<string, TokenType>());
                 Symboltable[Symboltable.Count - 1].Add(name, type);
                 //Add name, level, open, close param
-                ScopeTracker.Add(name, Tuple.Create(CurrentLevel,true, false));
-            
+                ScopeTracker.Add(name, Tuple.Create(CurrentLevelProp,true, false));
+
             }
-            //if (DicSymbolTable.TryAdd(name, type))
-            //{
-            //    DicSymbolTable.Add(name, type);
-            //}
+            else
+            {
+                //add to current symboltable
+                Symboltable[Symboltable.Count - 1].Add(name, type);
+            }
+
             throw new InvalidSyntaxException($"Symbol {name} was not added in symbol table, since it is there already");
         }
         public bool LookUp(string Name, int Level)
         {
 
-            if (ScopeTracker.ContainsKey(Tuple.Create(Name, Level)))
+            if (ScopeTracker.ContainsKey(Name))
             {
                 throw new InvalidSyntaxException($"Symbol { Name } was not added in the symbol table, since it exist already at scope {Level}");
             }
-            return true;
+            return false;
         }
     }
 }
