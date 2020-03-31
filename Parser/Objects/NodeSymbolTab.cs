d45aaae9d411dff3fd52376110f3d58750fe7487
smh87 <spm2014@hotmail.com>
2020-03-31 15:04:47 +0200

    node symboltable

---
+++ Parser/Objects/NodeSymbolTab.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lexer.Objects;
+
+namespace Parser.Objects
+{
+    /// <summary>
+    /// Symbol table node
+    /// </summary>
+    class NodeSymbolTab
+    {
+        List<NodeSymbolTab> ChildrenList = new List<NodeSymbolTab>();
+        public NodeSymbolTab Parent { get; set; }
+       
+        string name;
+        TokenType type;
+
+        //TODO ID, Add children, Openscope, Closescope Add parent,
+        
+    }
+}
