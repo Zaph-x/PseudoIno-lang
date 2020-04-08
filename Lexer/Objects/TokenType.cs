3183250e353251444c03b6ebf0ea797139315079
sejbas <markamb2497@gmail.com>
2020-04-08 09:57:52 +0200

    Added xml comments to tokentypes

--- Lexer/Objects/TokenType.cs
+++ Lexer/Objects/TokenType.cs
@@ -207,36 +207,129 @@ namespace Lexer.Objects
         EOF,
         
         //Parser
+        /// <summary>
+        /// Program token, non terminal
+        /// </summary>
         PROG,
+        /// <summary>
+        /// Start token, non terminal
+        /// </summary>
         START,
+        /// <summary>
+        /// Statements token, non terminal
+        /// </summary>
         STMNTS,
+        /// <summary>
+        /// Statement token, non terminal
+        /// </summary>
         STMNT,
+        /// <summary>
+        /// Expression token, non terminal
+        /// </summary>
         EXPR,
+        /// <summary>
+        /// Math expression token, non terminal
+        /// </summary>
         MATHEXPR,
+        /// <summary>
+        /// Math operation token, non terminal
+        /// </summary>
         MATH_OP,
+        /// <summary>
+        /// Array init token, non terminal
+        /// </summary>
         ARRINIT,
+        /// <summary>
+        /// Boolean expression token, non terminal
+        /// </summary>
         BOOLEXPR,
+        /// <summary>
+        /// Boolean operation token, non terminal
+        /// </summary>
         BOOL_OP,
+        /// <summary>
+        /// If statement token, non terminal
+        /// </summary>
         IFSTMNT,
+        /// <summary>
+        /// Pin token, non terminal
+        /// </summary>
         PIN,
+        /// <summary>
+        /// Function call token, non terminal
+        /// </summary>
         FUNCCALL,
+        /// <summary>
+        /// Argument list token, non terminal
+        /// </summary>
         ARGLIST,
+        /// <summary>
+        /// Function token, non terminal
+        /// </summary>
         FUNCTION,
+        /// <summary>
+        /// Function statement token, non terminal
+        /// </summary>
         FUNCSTMNT,
+        /// <summary>
+        /// Function statements token, non terminal
+        /// </summary>
         FUNCSTMNTS,
+        /// <summary>
+        /// Code block token, non terminal
+        /// </summary>
         CODEBLOCK,
+        /// <summary>
+        /// End function token, non terminal
+        /// </summary>
         ENDFUNC,
+        /// <summary>
+        /// Begin statement token, non terminal
+        /// </summary>
         BEGINSTMNT,
+        /// <summary>
+        /// Loop while token, non terminal
+        /// </summary>
         LOOPW,
+        /// <summary>
+        /// Loop for token, non terminal
+        /// </summary>
         LOOPF,
+        /// <summary>
+        /// End while token, non terminal
+        /// </summary>
         ENDWHILE,
+        /// <summary>
+        /// End for token, non terminal
+        /// </summary>
         ENDFOR,
+        /// <summary>
+        /// Or equal token, non terminal
+        /// </summary>
         OREQUAL,
+        /// <summary>
+        /// Assignment token, non terminal
+        /// </summary>
         ASSIGNMENT,
+        /// <summary>
+        /// Type token, non terminal
+        /// </summary>
         TYPE,
+        /// <summary>
+        /// Argument token, non terminal
+        /// </summary>
         ARG,
+        /// <summary>
+        /// Optional arguments token, non terminal
+        /// </summary>
         OPTNL_ARGS,
+        /// <summary>
+        /// Beginable token, non terminal
+        /// </summary>
         BEGINABLE,
+        /// <summary>
+        /// Array token, non terminal
+        /// </summary>
         ARR
     }
 }
