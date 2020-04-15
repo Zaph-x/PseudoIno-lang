f2ff902f78b030d84daebc4b7083485fe8203fd4
Zaph-x <35431127+Zaph-x@users.noreply.github.com>
2020-04-15 09:42:52 +0200

    Made this better too
    Signed-off-by: Zaph-x <35431127+Zaph-x@users.noreply.github.com>

--- tools/travis/CompileTest.ps1
+++ tools/travis/CompileTest.ps1
@@ -1,7 +1,7 @@
 $countPass = (Get-ChildItem -File -Path ".\premade-programs\pass\" | Measure-Object).Count
 $countFail = (Get-ChildItem -File -Path ".\premade-programs\fail\" | Measure-Object).Count
-$exePath = "..\Core\bin\Debug\netcoreapp3.1\"
-$pathBack = "../../../.."
+$exePath = "$TRAVIS_BUILD_DIR\Core\bin\Debug\netcoreapp3.1\"
+$pathBack = "$TRAVIS_BUILD_DIR"
 function ExpectError() {
     param(
         $exitcode,
