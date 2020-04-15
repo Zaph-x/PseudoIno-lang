06e1b75d759be2ed59b9295e51285ad7090edd30
sejbas <markamb2497@gmail.com>
2020-04-15 16:36:40 +0200

    Work started on cfg reader

---
+++ Parser/Objects/CfgReader.cs
@@ -0,0 +1,102 @@
+using System.Collections.Generic;
+
+namespace Parser.Objects
+{
+    public class CfgReader
+    {
+        private bool flag = false;
+        List<GrammarRule> GrammarRules = new List<GrammarRule>();
+        
+        public void GrammarScanner()
+        {
+            int counter = 0;  
+            string line;  
+              
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"cfg.md");  
+            while((line = file.ReadLine()) != null)  
+            {  
+                LineScanner(line);
+                System.Console.WriteLine(line);  
+                counter++;  
+            }  
+              
+            file.Close();  
+            System.Console.WriteLine("There were {0} lines.", counter);  
+            // Suspend the screen.  
+            System.Console.ReadLine();  
+        }
+
+        public void LineScanner(string line)
+        {
+            if (line.StartsWith(' ') || line.StartsWith('\n'))
+            {
+                flag = true;
+                return;
+            }
+            else if (line.StartsWith('|'))
+            {
+                
+            }
+            else
+            {
+                
+            }
+        }
+    }
+
+    public class GrammarRule
+    {
+        public NotTerminal Name;
+        List<Production> productions = new List<Production>();
+
+        public GrammarRule(string name)
+        {
+            Name = new NotTerminal(name);
+        }
+    }
+    
+    public class Production
+    {
+        public int Type;
+        public int RuleNumber;
+        public Terminal Terminals;
+        public NotTerminal NonTerminals;
+
+        public Production(string production, int ruleNumber)
+        {
+            RuleNumber = ruleNumber;
+            if (production.StartsWith('\''))
+            {
+                Type = 0;
+                Terminals = new Terminal(production);
+            }
+            else
+            {
+                Type = 1;
+                NonTerminals = new NotTerminal(production);
+            }
+        }
+    }
+
+    public class Terminal
+    {
+        public string Name { get; set; }
+
+        public Terminal(string name)
+        {
+            Name = name;
+        }
+    }
+    
+    public class NotTerminal
+    {
+        public string Name { get; set; }
+        
+        public NotTerminal(string name)
+        {
+            Name = name;
+        }
+    }
+}
