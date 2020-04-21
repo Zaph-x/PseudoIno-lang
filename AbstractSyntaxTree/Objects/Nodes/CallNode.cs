41a5dde22498a562ca281630e802175c9f969125
sejbas <markamb2497@gmail.com>
2020-04-21 22:27:43 +0200

    Added logic to call and call parameters nodes

--- AbstractSyntaxTree/Objects/Nodes/CallNode.cs
+++ AbstractSyntaxTree/Objects/Nodes/CallNode.cs
@@ -2,8 +2,10 @@ using System;
 using Lexer.Objects;
 namespace AbstractSyntaxTree.Objects.Nodes
 {
-    public class CallNode : AstNode
+    public class CallNode : StatementNode
     {
+        public VarNode VarNode { get; set; }
+        public CallParametersNode RightHand { get; set; }
         public CallNode(int line, int offset) : base(TokenType.CALL, line, offset)
         {
         }
---
+++ AbstractSyntaxTree/Objects/Nodes/CallParametersNode.cs
@@ -0,0 +1,18 @@
+using Lexer.Objects;
+
+namespace AbstractSyntaxTree.Objects.Nodes
+{
+    public class CallParametersNode : AstNode
+    {
+        public ValNode ValNode { get; set; }
+        public CallParametersNode RightHand { get; set; }
+        public CallParametersNode(int line, int offset) : base(TokenType.CALLPARAMS, line, offset)
+        {
+        }
+
+        public override void Accept(Visitor visitor)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
