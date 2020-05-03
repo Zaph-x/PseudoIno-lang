450e62efcfe4df5d8b4501e82d450c84a290ddf8
sejbas <markamb2497@gmail.com>
2020-05-03 15:15:17 +0200

    Renamed NodeSymbolTab to SymbolTable

--- SymbolTable/SymbolTable.cs
+++ SymbolTable/SymbolTable.cs
@@ -13,61 +13,19 @@ namespace SymbolTable
     /// <summary>
     /// Symbol table node
     /// </summary>
-    public class NodeSymbolTab
+    public class SymbolTable
     {
-        /// <summary>
-        /// Children list 
-        /// </summary>
-        public List<NodeSymbolTab> ChildrenList = new List<NodeSymbolTab>();
+        
         public List<AstNode> Symbols = new List<AstNode>();
-
-        /// <summary>
-        /// Parent property
-        /// </summary>
-        public NodeSymbolTab Parent { get; set; }
         
-        //Id
-        public Guid Id = Guid.NewGuid();
+        public SymbolTable Parent { get; set; }
         
-        //Name and Type for the node
-        public string Name,Value;
-        public TokenType Type;
-
-        /// <summary>
-        /// Line og offset for every token
-        /// </summary>
-        public long Line;
-        public int Offset;
-
-        /// <summary>
-        /// Constructor to add Name,Type, Line and offset attributes for children.
-        /// </summary>
-        /// <param name="name"></param>
-        /// <param name="token"></param>
-        public NodeSymbolTab(AstNode token)
+        public SymbolTable(AstNode token)
         {
-            this.Value = token.Value;
-            this.Type = token.Type;
-            //this.Name = name;
-            this.Line = token.Line;
-            this.Offset = token.Offset;
-           
-        }
-        /// <summary>
-        /// constructer overloading for use 
-        /// </summary>
-        public NodeSymbolTab()
-        {
-       
-        }
-        //list af tokentype ind 
-        //list af scanner token ud
 
-        /// <summary>
-        /// Add child to Childrenlist. Set parent property of the child node. Input parameters are Name and Type of the node.
-        /// </summary>
-        /// <param Name="token"></param>
-        public void AddNode(AstNode token, NodeSymbolTab symbolTable)
+        }
+        
+        public void AddNode(AstNode token, SymbolTable symbolTable)
         {
             if (TokenTypeExpressions.IsBlock(token.Type))
             {
@@ -83,14 +41,14 @@ namespace SymbolTable
             }
         }
 
-        public void AddBlock(AstNode node, NodeSymbolTab symbolTab)
+        public void AddBlock(AstNode node, SymbolTable symbolTable)
         {
             if (node.Type == TokenType.FUNC)
             {
                 throw new Exception("No functions in functions please!");
             }
 
-            ChildrenList.Add(new NodeSymbolTab(node) { Parent = symbolTab});
+            ChildrenList.Add(new SymbolTable(node) { Parent = symbolTable});
             //if (this.Parent != null)
             //{
             //    if (Parent.ChildrenList.Any(x => x.Id == this.Id && x.ChildrenList.Count == 0))
@@ -152,35 +110,6 @@ namespace SymbolTable
             return Symbols.Any(child => GetNameFromRef(child) == name);
         }
         
-        /*public void AddStatementNode(StatementNode statementNode)
-        {
-            string name = "";
-            AstNode astNode = new AndNode(1,1);
-            if (statementNode.Type == TokenType.ASSIGNMENT)
-            {
-                name = ((AssignmentNode) statementNode).Var.Id;
-                astNode = (AstNode) statementNode;
-            }
-            else if (statementNode.Type == TokenType.CALL)
-            {
-                //name = ((CallNode) statementNode).Var.Id;
-                astNode = (AstNode) statementNode;
-            }
-
-            if (astNode.Type == TokenType.OP_AND)
-            {
-                throw new Exception("nope");
-            }
-            
-            if (!ChildrenList.Any(x => x.Name == name && x.Type == astNode.Type))
-            {
-                ChildrenList.Add(new NodeSymbolTab(name, astNode) { Parent = this });
-            }
-            else
-            {
-                throw new Exception($"Symbol table contains the Name {name}");
-            }
-        }*/
     }
 }
 
