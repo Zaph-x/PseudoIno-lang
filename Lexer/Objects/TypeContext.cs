723f2335b25081ebc8ea85ad396d836d61dc5c16
Zaph-x <mikkel@luceartfoto.dk>
2020-05-05 15:11:58 +0200

    Added var type

--- Lexer/Objects/TypeContext.cs
+++ Lexer/Objects/TypeContext.cs
@@ -4,16 +4,18 @@ namespace Lexer.Objects
 {
     public class TypeContext
     {
-        private TokenType _tokenType {get;set;}
-        public TokenType Type {get => _tokenType;}
+        private TokenType _tokenType { get; set; }
+        public TokenType Type { get => _tokenType; }
 
-        public TypeContext(TokenType type) {
+        public TypeContext(TokenType type)
+        {
             ValidateType(type);
         }
 
-        private void ValidateType(TokenType type) 
+        private void ValidateType(TokenType type)
         {
-            switch (type) {
+            switch (type)
+            {
                 case TokenType.NUMERIC:
                 case TokenType.BOOL:
                 case TokenType.STRING:
@@ -21,11 +23,20 @@ namespace Lexer.Objects
                 case TokenType.DPIN:
                 case TokenType.ARR:
                 case TokenType.FUNC:
+                case TokenType.VAR:
                     _tokenType = type;
                     break;
                 default:
                     throw new InvalidTypeException($"Type {type} is invalid.");
             }
+        }
+
+        // override object.Equals
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() == typeof(TypeContext))
+                return this.Type == ((TypeContext)obj).Type;
+            else return false;
         }
     }
 }
