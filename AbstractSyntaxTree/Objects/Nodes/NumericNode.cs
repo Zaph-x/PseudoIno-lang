d09a7322f3911409b569a488395273ef793d9524
smh87 <spm2014@hotmail.com>
2020-05-13 16:56:08 +0200

    rigtigt tal format i numeric-node

--- AbstractSyntaxTree/Objects/Nodes/NumericNode.cs
+++ AbstractSyntaxTree/Objects/Nodes/NumericNode.cs
@@ -1,5 +1,5 @@
 using Lexer.Objects;
-
+using System.Globalization;
 namespace AbstractSyntaxTree.Objects.Nodes
 {
     public class NumericNode : ValNode
@@ -9,9 +9,13 @@ namespace AbstractSyntaxTree.Objects.Nodes
         public int IValue {get;set;}
         public NumericNode(string value , ScannerToken token) : base(token)
         {
+            System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+            customCulture.NumberFormat.NumberDecimalSeparator = ".";
+            System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
             float _f;
             int _i;
-            float.TryParse(value, out _f);
+           // float.TryParse(value, CultureInfo.InvariantCulture.,, out _f);
+            _f = float.Parse(value, customCulture);
             int.TryParse(value, out _i);
 
             FValue = _f;
