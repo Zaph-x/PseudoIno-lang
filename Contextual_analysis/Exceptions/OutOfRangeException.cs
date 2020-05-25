b7465e3bc943de37dbad8e59002ad71223ce6698
Zaph-x <35431127+Zaph-x@users.noreply.github.com>
2020-05-25 20:43:26 +0200

    Finished docs
    Signed-off-by: Zaph-x <35431127+Zaph-x@users.noreply.github.com>

--- Contextual_analysis/Exceptions/OutOfRangeException.cs
+++ Contextual_analysis/Exceptions/OutOfRangeException.cs
@@ -2,8 +2,15 @@ using AbstractSyntaxTree.Objects;
 using System;
 namespace Contextual_analysis.Exceptions
 {
+    /// <summary>
+    /// An exception to raise when an array index is out of range
+    /// </summary>
     public class OutOfRangeException : Exception
     {
+        /// <summary>
+        /// An exception to raise when an array is indexed with an index that is out of range.
+        /// </summary>
+        /// <param name="message">The message to show the user</param>
         public OutOfRangeException(string message) : base(message)
         {
             Console.Error.WriteLine(message);
